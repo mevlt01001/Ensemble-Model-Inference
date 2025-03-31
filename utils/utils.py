f7a6009bf4c19cbbdc608b2615ee1d4503a786ea
mevlt01001 <mevlutbasaran01@gmail.com>
2025-04-01 01:08:32 +0300

    utils updated

--- utils/utils.py
+++ utils/utils.py
@@ -47,9 +47,14 @@ def get_all_images(imgs=640):
 
 def get_data(name, imgs=640):
     image = get_image(name, imgs)
-    data = np.expand_dims(image, axis=0)
-    data = np.reshape(data, (1, 3, imgs, imgs))
-    return data
+    data = np.transpose(image, (2, 0, 1))#hwc2chw
+    data = np.expand_dims(data, axis=0).astype(np.float32)
+    return data/255.0
+
+def preprocess(image, imgs=640):
+    data = np.transpose(image, (2, 0, 1))#hwc2chw
+    data = np.expand_dims(data, axis=0).astype(np.float32)
+    return data/255.0
 
 def _get_all_data(imgs=640):
     return np.array([get_data(name, imgs) for name in names])
