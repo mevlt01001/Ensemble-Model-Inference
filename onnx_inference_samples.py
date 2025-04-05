330076eb0375ad98c5d272aa0dbb59b60fa4ff27
mevlt01001 <mevlutbasaran01@gmail.com>
2025-04-05 17:34:25 +0300

    onnx inference samples added

--- onnx_inference_samples.py
+++ onnx_inference_samples.py
@@ -1,7 +1,6 @@
 from utils import get_names, get_image, get_data, plot_image, create_onnx_session, onnxruntime_inference
 
 names = get_names()
-
 onnx_path = "YOLO12-RTDETR_ensemble_model.onnx"
 input_data = get_data(names[0])
 providers = ['CUDAExecutionProvider']
